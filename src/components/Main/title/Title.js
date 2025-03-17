49e19dff772b779b2fc1695665f203044da7ec91
fausterros <fausteros@mail.ru>
2025-03-17 20:08:49 +0700

    Add files via upload

---
+++ src/components/Main/title/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './title.css';
+function Title(){
+    return(
+        <div>
+            <h1 className="title">Меню</h1>
+        </div>
+    );
+}
+export default Title;
